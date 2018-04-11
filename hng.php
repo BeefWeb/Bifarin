d5a3a1b3186a42287e479749dee3d57d8bb90321
BeefWeb <Alayonimibifarin@yahoo.com>
2018-04-11 14:03:19 +0100

    First Commit

---
+++ hng.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link href="https://fonts.googleapis.com/css?family=Seaweed+Script" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Text+Me+One" rel="stylesheet">
+<style>
+body {background-color:#2D9CDB;}
+h1   {color:White;
+      text-align:Centre; 
+	  font-family: 'Seaweed Script', cursive;
+  }
+.boxed {
+  border: 1px solid #F2F2F2;
+  background-color: #F2F2F2;
+  padding: 10px;
+  width: 330px;
+  height:75px;
+   margin: auto;
+   text-align: center;
+   font-family: 'Text Me One', sans-serif;
+   font-size: 3
+}
+</style>
+</head>
+<body>
+
+<Font color ="White" size="6.5" face ="Seaweed Script"><h1 align="center">Hello World!</h1></font>
+<Font color ="#F2C94C" size="3" face ="Seaweed Script"><h1 align="center">HNG Internship 2018</h1></font>  
+<div class="boxed">
+  <?php
+echo "Tue April 11, " . date("h:i:sa");
+?>
+</div>
+
+</body>
+</html>
